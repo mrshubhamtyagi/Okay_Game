596c4ff8f5e7e2275dc99d7803570686a5fa3a31
Shubham Tyagi <styagi.2301@gmail.com>
2020-04-19 01:15:39 +0530

    Just Started

---
+++ Okay/Assets/_MyStuff/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+
+    private void Awake() => Instance = this;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
